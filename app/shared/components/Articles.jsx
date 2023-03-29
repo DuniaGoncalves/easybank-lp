e11df3a3d3c7c3cde332dae81bce3861e5849a2c
Dunia Goncalves <dgoncalves@everquote.com>
2023-03-29 04:52:01 -0400

    map through arr of obj to get content on page

--- app/shared/components/Articles.jsx
+++ app/shared/components/Articles.jsx
@@ -1,35 +1,49 @@
-import { Link } from "@remix-run/react";
-// import logoSVG from "~/public/images/logo";
+// Can we map through each column and pull in data maybe with prisma
+const blog = [
+  {
+    image: '../../images/image-currency.jpg',
+    author: 'By Claire Robinson',
+    title: 'Receive money in any currency with no fees',
+    content:
+      'The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …',
+  },
+  {
+    image:'../../images/image-restaurant.jpg',
+    author: 'By Wilson Hutton',
+    title: 'Treat yourself without worrying about money',
+    content:
+      'Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …',
+  },
+  {
+    image: '../../images/image-plane.jpg',
+    author: 'By Wilson Hutton',
+    title: 'Take your Easybank card wherever you go',
+    content:
+      'We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …',
+  },
+  {
+    image: '../../images/image-confetti.jpg',
+    author: 'By Claire Robinson',
+    title: 'Our invite-only Beta accounts are now live!',
+    content:
+      'After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ...',
+  },
+]
 
 export default function Articles() {
   return (
-    <section>
+    <section className="w-full bg-slate-50">
       <h2>Latest Articles</h2>
-      <article>
-        <img src="" alt="" />
-        <span>By Claire Robinson</span>
-        <h5>Receive money in any currency with no fees</h5>
-        <p>
-          The world is getting smaller and we’re becoming more mobile. So why should you be 
-          forced to only receive money in a single …
-        </p>
-      </article>
-
-
-By Wilson Hutton
-Treat yourself without worrying about money
-Our simple budgeting feature allows you to separate out your spending and set 
-realistic limits each month. That means you …
-
-By Wilson Hutton
-Take your Easybank card wherever you go
-We want you to enjoy your travels. This is why we don’t charge any fees on purchases 
-while you’re abroad. We’ll even show you …
-
-By Claire Robinson
-Our invite-only Beta accounts are now live!
-After a lot of hard work by the whole team, we’re excited to launch our closed beta. 
-It’s easy to request an invite through the site ...
+      <div className='flex flex-row justify-around w-full'> 
+        {blog.map((post) => (
+          <article key={post.title} className="w-80">
+            <img src={post.image} alt=''/>
+            <span>{post.author}</span>
+            <h5>{post.title}</h5>
+            <p>{post.content}</p>
+          </article>
+        ))}  
+      </div> 
     </section>
   );
 }
--- app/shared/components/Info.jsx
+++ app/shared/components/Info.jsx
@@ -1,77 +1,46 @@
-// Can we map through each column and pull in data maybe with prisma
 const bankingOptions = [
   {
-    icon: 'Calvin Hawkins',
-    title: 'calvin.hawkins@example.com',
+    icon: '../../images/icon-online.svg',
+    title: 'Online Banking',
     content:
-      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      'Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.',
   },
   {
-    icon: 'Kristen Ramos',
-    title: 'kristen.ramos@example.com',
+    icon:'../../images/icon-budgeting.svg',
+    title: 'Simple Budgeting',
     content:
-      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      'See exactly where your money goes each month. Receive notifications when you&apos;re close to hitting your limits.',
   },
   {
-    icon: 'Ted Fox',
-    title: 'ted.fox@example.com',
+    icon: '../../images/icon-onboarding.svg',
+    title: 'Fast Onboarding',
     content:
-      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      'We don&apos;t do branches. Open your account in minutes online and start taking control of your finances right away.',
   },
   {
-    icon: 'Ted Fox',
-    title: 'ted.fox@example.com',
+    icon: '../../images/icon-api.svg',
+    title: 'Open API',
     content:
-      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      'Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.',
   },
 ]
 
-// export default function Example() {
-//   return (
-//     <ul className="divide-y divide-gray-200">
-//       {people.map((person) => (
-//         <li key={person.email} className="py-4 flex">
-//           <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
-//           <div className="ml-3">
-//             <p className="text-sm font-medium text-gray-900">{person.name}</p>
-//             <p className="text-sm text-gray-500">{person.email}</p>
-//           </div>
-//         </li>
-//       ))}
-//     </ul>
-//   )
-// }
 export default function Info() {
   return (
-    <section className="divide-y divide-gray-200">
+    <section className="bg-slate-100">
         <h2>Why choose Easybank?</h2>
-
         <p>We leverage Open Banking to turn your bank account into your financial hub. Control 
         your finances like never before.</p>
         
-        {bankingOptions.map((option) => (
-          <article key={option.content} className="py-4 flex">
-              <img className="h-10 w-10 rounded-full" src={option.icon} alt="" />
-              <h4 className="text-sm font-medium text-gray-900">{option.title}</h4>
-              <p className="text-sm text-gray-500">{option.content}</p>  
-          </article>
-        ))}    
-          
-        Online Banking    
-         Our modern web and mobile applications allow you to keep track of your finances 
-              wherever you are in the world.
-
-        Simple Budgeting
-        See exactly where your money goes each month. Receive notifications when you’re 
-        close to hitting your limits.
-
-        Fast Onboarding
-        We don’t do branches. Open your account in minutes online and start taking control 
-        of your finances right away.
-
-        Open API
-        Manage your savings, investments, pension, and much more from one account. Tracking 
-        your money has never been easier.
+        <div className="flex flex-row">        
+          {bankingOptions.map((option) => (
+            <article key={option.title} className="py-4">
+                <img className="h-10 w-100" src={option.icon} alt="" />
+                <h4 className="text-sm font-medium text-gray-900">{option.title}</h4>
+                <p className="text-sm text-gray-500">{option.content}</p>  
+            </article>
+          ))}    
+        </div>
     </section>
   );
 }
