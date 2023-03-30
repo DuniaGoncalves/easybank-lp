857e91471696c0ec6771137f1db7566bcd6d5bb8
Dunia Goncalves <dgoncalves@everquote.com>
2023-03-30 00:55:16 -0400

    create a button component

---
+++ app/shared/components/Button.jsx
@@ -0,0 +1,7 @@
+export default function Button() {
+  return (
+    <button class="py-1 px-8 h-10 w-40 text-white text-sm rounded-full bg-gradient-to-r from-green-500 to-blue-400 hover:from-green-300 hover:to-blue-300">
+      Request Invite
+    </button>
+  )
+};
