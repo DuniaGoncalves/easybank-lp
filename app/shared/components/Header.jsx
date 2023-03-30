55fc89ed11d434c873432c812464b415f0589df5
Dunia Goncalves <dgoncalves@everquote.com>
2023-03-30 00:58:18 -0400

    refactor and style header for desktop

--- app/shared/components/Header.jsx
+++ app/shared/components/Header.jsx
@@ -1,21 +1,21 @@
-import { Link } from "@remix-run/react";
-// import logoSVG from "../../images/logo.svg";
-
+import Button from "~/shared/components/Button";
 
 export default function header() {
   return (
-    <header>
-      <nav>
-        <Link to={"/"} prefetch="none">
-          <img src="../../images/logo.svg" alt="logo" />
-        </Link>
-        <Link className="text-zinc-400" to="/">Home</Link>{" "}
-        <Link className="text-zinc-400"to="/about">About</Link>{" "}
-        <Link className="text-zinc-400"to="/contact">Contact</Link>{" "}
-        <Link className="text-zinc-400" to="/blog">Blog</Link>{" "}
-        <Link className="text-zinc-400" to="/about">Careers</Link>{" "}
-        <button>Request Invite</button>
+    <header className="flex justify-between my-4 mx-8">
+      <img className="h-8 w-40" src="../../images/logo.svg" alt="logo" />
+      <nav className="flex sm:justify-center space-x-4">
+        {[
+          ['Home', '/'],
+          ['About', '/about'],
+          ['Contact', '/contact'],
+          ['Blog', '/blog'],
+          ['Careers', '/careers'],
+        ].map(([title, url]) => (
+          <a href={url} className="py-2 text-zinc-400 font-medium hover:underline hover:underline-offset-8 hover:decoration-green-500 hover:decoration-4 hover:text-slate-600">{title}</a>
+        ))}
       </nav>
+      <Button />
     </header>
   );
 }
