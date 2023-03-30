aff762ab459d5513163381da045b49d4f9bf0f30
Dunia Goncalves <dgoncalves@everquote.com>
2023-03-30 02:06:14 -0400

    WIP: style hero for desktop

--- app/shared/components/Hero.jsx
+++ app/shared/components/Hero.jsx
@@ -1,17 +1,19 @@
-// import { Link } from "@remix-run/react";
+import Button from "./Button";
 
 export default function Hero() {
   return (
-    <section className="hero w-full bg-fixed h-96 bg-auto bg-no-repeat bg-right flex bg-slate-50">
-      <div>
-        <h1>Next generation digital banking</h1>
-        <p>
+    <section className="hero flex h-96 w-full bg-auto bg-no-repeat bg-[bottom_-10rem_right_-10rem] bg-slate-50">
+      <div className="m-auto w-1/3 px-8">
+        <h1 className="text-6xl font-medium text-slate-900 text-left mb-4">Next generation digital banking</h1>
+        <p className="mb-4">
           Take your financial life online. Your Easybank account will be a one-stop-shop 
           for spending, saving, budgeting, investing, and much more.
         </p>
-        <button>Request Invite</button>
+        <Button />
       </div>
-      <img src="../../images/image-mockups.png" alt="" />
+      <div className="w-1/2">
+        <img src="../../images/image-mockups.png" alt="" />
+      </div>
     </section>
   );
 }
