36d0c9745e41383e595d7f2a74750a70248d1110
Dunia Goncalves <dgoncalves@everquote.com>
2023-04-01 00:16:15 -0400

    style articles for desktop

--- app/shared/components/Articles.jsx
+++ app/shared/components/Articles.jsx
@@ -5,7 +5,7 @@ const blog = [
     author: 'By Claire Robinson',
     title: 'Receive money in any currency with no fees',
     content:
-      'The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …',
+      'The world is getting smaller and we\'re becoming more mobile. So why should you be forced to only receive money in a single …',
   },
   {
     image:'../../images/image-restaurant.jpg',
@@ -19,28 +19,30 @@ const blog = [
     author: 'By Wilson Hutton',
     title: 'Take your Easybank card wherever you go',
     content:
-      'We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …',
+      'We want you to enjoy your travels. This is why we don\'t charge any fees on purchases while you\'re abroad. We\'ll even show you …',
   },
   {
     image: '../../images/image-confetti.jpg',
     author: 'By Claire Robinson',
     title: 'Our invite-only Beta accounts are now live!',
     content:
-      'After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ...',
+      'After a lot of hard work by the whole team, we\'re excited to launch our closed beta. It\'s easy to request an invite through the site ...',
   },
 ]
 
 export default function Articles() {
   return (
     <section className="w-full bg-slate-50">
-      <h2>Latest Articles</h2>
-      <div className='flex flex-row justify-around w-full'> 
+      <h2 className="text-4xl font-normal text-slate-900 text-left mb-10 ml-36 mt-36 w-1/3 px-8">Latest Articles</h2>
+      <div className='flex flex-row justify-around mx-36 my-20'> 
         {blog.map((post) => (
-          <article key={post.title} className="w-80">
-            <img src={post.image} alt=''/>
-            <span>{post.author}</span>
-            <h5>{post.title}</h5>
-            <p>{post.content}</p>
+          <article key={post.title} className="w-3/12 px-10">
+            <img src={post.image} alt='article image'/>
+            <div className="bg-white pb-10 pt-2 px-10">
+              <span className="mb-2 text-xs text-gray-500">{post.author}</span>
+              <h5 className="mb-2 text-sm font-medium text-gray-900">{post.title}</h5>
+              <p className="text-xs text-gray-500">{post.content}</p>
+            </div>
           </article>
         ))}  
       </div> 
