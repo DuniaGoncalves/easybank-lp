b163943c45fdee8fa833b208791d7e43245a508c
Dunia Goncalves <dgoncalves@everquote.com>
2023-03-31 23:58:54 -0400

    style info for desktop

--- app/shared/components/Info.jsx
+++ app/shared/components/Info.jsx
@@ -9,13 +9,13 @@ const bankingOptions = [
     icon:'../../images/icon-budgeting.svg',
     title: 'Simple Budgeting',
     content:
-      'See exactly where your money goes each month. Receive notifications when you&apos;re close to hitting your limits.',
+      'See exactly where your money goes each month. Receive notifications when you\'re close to hitting your limits.',
   },
   {
     icon: '../../images/icon-onboarding.svg',
     title: 'Fast Onboarding',
     content:
-      'We don&apos;t do branches. Open your account in minutes online and start taking control of your finances right away.',
+      'We don\'t do branches. Open your account in minutes online and start taking control of your finances right away.',
   },
   {
     icon: '../../images/icon-api.svg',
@@ -27,20 +27,21 @@ const bankingOptions = [
 
 export default function Info() {
   return (
-    <section className="bg-slate-100">
-        <h2>Why choose Easybank?</h2>
-        <p>We leverage Open Banking to turn your bank account into your financial hub. Control 
+    <section className="bg-slate-100 h-full w-full">
+      <div className="ml-36 mt-36 w-1/3 px-8">
+        <h2 className="text-4xl font-normal text-slate-900 text-left mb-4">Why choose Easybank?</h2>
+        <p className="text-zinc-400">We leverage Open Banking to turn your bank account into your financial hub. Control 
         your finances like never before.</p>
-        
-        <div className="flex flex-row">        
-          {bankingOptions.map((option) => (
-            <article key={option.title} className="py-4">
-                <img className="h-10 w-100" src={option.icon} alt="" />
-                <h4 className="text-sm font-medium text-gray-900">{option.title}</h4>
-                <p className="text-sm text-gray-500">{option.content}</p>  
-            </article>
-          ))}    
-        </div>
+      </div>
+      <div className="flex flex-row justify-around mx-36 my-20">        
+        {bankingOptions.map((option) => (
+          <article key={option.title} className="w-3/12 px-10">
+              <img className="mb-6 h-50 w-100 " src={option.icon} alt="" />
+              <h4 className="mb-2 text-lg font-medium text-gray-900">{option.title}</h4>
+              <p className="text-sm text-gray-500">{option.content}</p>  
+          </article>
+        ))}    
+      </div>
     </section>
   );
 }
