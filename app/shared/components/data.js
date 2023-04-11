1755e5490ebe8b31fa84dd42eff3722defad3012
Dunia Goncalves <dgoncalves@everquote.com>
2023-04-10 22:16:58 -0400

    extract data to seperate file

---
+++ app/shared/components/data.js
@@ -0,0 +1,61 @@
+export const bankingOptions = [
+  {
+    icon: '../../images/icon-online.svg',
+    title: 'Online Banking',
+    content:
+      'Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.',
+  },
+  {
+    icon:'../../images/icon-budgeting.svg',
+    title: 'Simple Budgeting',
+    content:
+      'See exactly where your money goes each month. Receive notifications when you\'re close to hitting your limits.',
+  },
+  {
+    icon: '../../images/icon-onboarding.svg',
+    title: 'Fast Onboarding',
+    content:
+      'We don\'t do branches. Open your account in minutes online and start taking control of your finances right away.',
+  },
+  {
+    icon: '../../images/icon-api.svg',
+    title: 'Open API',
+    content:
+      'Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.',
+  },
+]
+
+export const blog = [
+  {
+    image: '../../images/image-currency.jpg',
+    imageAlt: 'Currency from various countries',
+    author: 'By Claire Robinson',
+    title: 'Receive money in any currency with no fees',
+    content:
+      'The world is getting smaller and we\'re becoming more mobile. So why should you be forced to only receive money in a single …',
+  },
+  {
+    image:'../../images/image-restaurant.jpg',
+    imageAlt: 'Fancy plate setting which includes raw salmon, micro greens and puff pastry',
+    author: 'By Wilson Hutton',
+    title: 'Treat yourself without worrying about money',
+    content:
+      'Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …',
+  },
+  {
+    image: '../../images/image-plane.jpg',
+    imageAlt: 'View from an airplane window of the the plan\'s wing, pillowy clouds and a rising sun',
+    author: 'By Wilson Hutton',
+    title: 'Take your Easybank card wherever you go',
+    content:
+      'We want you to enjoy your travels. This is why we don\'t charge any fees on purchases while you\'re abroad. We\'ll even show you …',
+  },
+  {
+    image: '../../images/image-confetti.jpg',
+    imageAlt: 'Rainbow confetti falling with a teal background',
+    author: 'By Claire Robinson',
+    title: 'Our invite-only Beta accounts are now live!',
+    content:
+      'After a lot of hard work by the whole team, we\'re excited to launch our closed beta. It\'s easy to request an invite through the site ...',
+  },
+]
